3253935446fd9e78707ec4ab68852919f932c97a
Shreya Singhal <114510444+shreyasinghal1111@users.noreply.github.com>
2024-10-19 21:52:50 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id:428,
+    fullname:"hacktober",
+    username:"https://github.com/hacktober",
+  }
   
 ];
